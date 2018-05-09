0dab376b22657c195763fdf1948bef27336122c4
Anthony Lam <al3101@nyu.edu>
2018-05-09 22:55:33 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,10 +8,10 @@ def run_guessing_game
     if input.to_i == random_num
       puts "You guessed the correct number!"
     elsif input == "exit"
+      puts "Goodbye!"
       break
     else
       puts "The computer guessed #{random_num}."
     end
   end
-  puts "Goodbye!"
 end
